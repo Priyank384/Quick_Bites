ae6e91a64c9e3b91661478024266355a966d699f
Priyank384 <priyankgupta384@gmail.com>
2023-09-02 23:54:23 +0530

    updated login page

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -24,6 +24,30 @@ router.post("/createuser", [
             console.log(error)
             res.json({ success: false });
         }
+    }
+)
+router.post("/loginuser", [
+    body('email', 'invalid e-mail').isEmail(),
+    body('password', 'password should be minimum of 5 characters').isLength({ min: 5 })],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        let email = req.body.email;
+        try {
+            let userData = await User.findOne({ email });
+            if (!userData) {
+                return res.status(400).json({ errors: "Try logging with correct credentials" });
+            }
+            if (req.body.password !== userData.password) {
+                return res.status(400).json({ errors: "Try logging with correct credentials" });
+            }
+            return res.json({ success: true });
+        } catch (error) {
+            console.log(error)
+            res.json({ success: false });
+        }
     })
 
 module.exports = router;
